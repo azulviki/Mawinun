eb89223edb6a41940d9c6347ba1e063f06afe4e5
azulviki <azulesvictoria@gmail.com>
2026-09-20 16:17:18 -0300

    tiempo arboles

--- sketch.js
+++ sketch.js
@@ -38,7 +38,7 @@ const JUNTAR_DECAIMIENTO = 0.04;
 const TIEMPO_MINIMO_PANTALLA = 1000; // 1 segundo de resguardo tras cambio de escena
 
 //MODO COMPLEJO
-const PROBABILIDAD_REENCENDIDO = 0.0002; // Una vez cada pocos segundos, por árbol apagado
+const PROBABILIDAD_REENCENDIDO = 0.0008; // Una vez cada pocos segundos, por árbol apagado
 const MAX_REENCENDIDOS = 4; // Máximo de veces que un árbol puede reencenderse
 
 let cantidadManosDetectadas = 0;
